4a1cdcb3ae44e2b8e33436a4a3c2119cf16d14bb
Matthew Selwyn <157687386+123kiwi@users.noreply.github.com>
2025-01-24 09:06:48 +0100

    Update Streamlit_to_Github.py

--- Streamlit_to_Github.py
+++ Streamlit_to_Github.py
@@ -1,4 +1,28 @@
+# Import the necessary libraries
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Add a title
 st.title("Air BnB : A Data-Driven Analysis")
 st.write("A Streamlit project by MM, EB & MS.")
+st.sidebar.title("Table of contents")
+# Create three pages
+pages=["Exploration", "DataVizualization", "Modelling"]
+page=st.sidebar.radio("Go to", pages)
+
+# Write "Presentation of data" at the top of the first page
+if page == pages[0] : 
+  st.write("### Presentation of data")
+  
+  
+# Write "DataVizualization" at the top of the second page
+if page == pages[1] : 
+  st.write("### DataVizualization")
+
+  
+# Write "Modelling" at the top of the third page
+if page == pages[2] : 
+  st.write("### Modelling")
